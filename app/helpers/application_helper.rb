3c6935e53c9c3dc5c622e4b8d6654faa58058458
Uday Kale <github@uday.net>
2022-02-07 22:31:13 -0600

    Add helper method for dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = " ")
+    default_title = "Charityplaza™ - Charity information portal"
+    if page_title.empty?
+      default_title
+    else
+      "#{page_title} | #{default_title}"
+    end
+  end
 end
