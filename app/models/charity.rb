e62e885fa36643a4fd97756518fda1ed61ddd766
Uday Kale <github@uday.net>
2022-07-21 21:18:28 -0500

    Add validations for charity model

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,3 +1,9 @@
 class Charity < ApplicationRecord
   has_rich_text :description
+  validates :title, presence: true, length: {minimum: 5}, uniqueness: true
+  validates :tax_id_number, uniqueness: true
+  validates :mission, presence: true
+  validates :address1, :city, :state, presence: true
+  validates :postal_code, presence: true, numericality: true, length: {is: 6}
+  validates :phone, presence: true, length: {minimum: 6, maximum: 12}
 end
