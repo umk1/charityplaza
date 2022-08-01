fe1a91a869aabbf72b43dd3a551503ff0b3e3b73
Uday Kale <github@uday.net>
2022-07-31 23:19:32 -0500

    fix for bin/dev command not working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "./controllers"
 import * as bootstrap from "bootstrap"
 import "trix"
 import "@rails/actiontext"
 import "@fortawesome/fontawesome-free/js/all"
-import "controllers"
-import "@hotwired/turbo-rails"
+
+//enable popover and tooltip ALWAYS.
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
